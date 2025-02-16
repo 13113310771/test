d972a4c7c95d073bbd924df3e5198782a15d68a3
jiang <3401345764@qq.com>
2025-02-17 00:23:24 +0800

    只有拍视频的代码

--- control-group-engineer-main - 12.20 拍视频/24编码控制器v2.2 c8t6 6轴完全体/receive_task.h
+++
@@ -1,9 +0,0 @@
-#ifndef _receive_task_H
-#define _receive_task_H
-
-#include "stm32f10x.h"                  // Device header
-
-
-
-
-#endif
--- control-group-engineer-main - 12.20 拍视频/BSP_CONF/bsp_trigonometry.h
+++
@@ -1,9 +0,0 @@
-#ifndef _bsp_trigonometry_H
-#define _bsp_trigonometry_H
-
-#include "stm32f4xx.h"
-#include "ramp.h"
-
-float fast_cos(int16_t angle);
-float fast_sin(int16_t angle);
-#endif
